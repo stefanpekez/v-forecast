1865c99cfae8e5ab9c77715068a66d9e2f6486da
Stefan Pekez <stefanpekez5@gmail.com>
2023-10-05 10:19:32 +0200

    Initial commit

---
+++ src/test/java/example/vforecast/VForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package example.vforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
