dcd79a22c42d80c18f5e6f71fce837df6af63de6
Stefan Pekez <stefanpekez5@gmail.com>
2023-10-06 17:39:56 +0200

    Implement forecast saving upon app startup

--- src/main/java/example/vforecast/service/impl/OpenWeatherMapServiceImpl.java
+++ src/main/java/example/vforecast/service/impl/OpenWeatherMapServiceImpl.java
@@ -1,8 +1,10 @@
 package example.vforecast.service.impl;
 
 import example.vforecast.dto.CityGetDto;
+import example.vforecast.dto.FiveDayForecastCreateDto;
 import example.vforecast.dto.OpenWeatherMapForecastGetDto;
 import example.vforecast.service.CityService;
+import example.vforecast.service.FiveDayForecastService;
 import example.vforecast.service.OpenWeatherMapService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,9 +21,11 @@ public class OpenWeatherMapServiceImpl implements OpenWeatherMapService {
     private String units;
 
     private final CityService cityService;
+    private final FiveDayForecastService forecastService;
 
-    public OpenWeatherMapServiceImpl(CityService cityService) {
+    public OpenWeatherMapServiceImpl(CityService cityService, FiveDayForecastService forecastService) {
         this.cityService = cityService;
+        this.forecastService = forecastService;
     }
 
     @Override
@@ -29,8 +33,8 @@ public class OpenWeatherMapServiceImpl implements OpenWeatherMapService {
     public void findFiveDayForecast() {
         WebClient webClient = WebClient.create();
 
-        for (CityGetDto cityGetDto: this.cityService.findAll()) {
-            String cityInfo = cityGetDto.name() + "," + cityGetDto.countryCode();
+        for (CityGetDto city: this.cityService.findAll()) {
+            String cityInfo = city.name() + "," + city.countryCode();
 
             WebClient.ResponseSpec responseSpec = webClient
                     .get()
@@ -38,6 +42,9 @@ public class OpenWeatherMapServiceImpl implements OpenWeatherMapService {
                     .retrieve();
 
             OpenWeatherMapForecastGetDto openWeatherMapForecastGetDto = responseSpec.bodyToMono(OpenWeatherMapForecastGetDto.class).block();
+
+            FiveDayForecastCreateDto forecastCreateDto = new FiveDayForecastCreateDto(city);
+            this.forecastService.save(forecastCreateDto);
         }
 
     }
