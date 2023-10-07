b41c7351f14b26b8f6664899f940c15e53b39789
Stefan Pekez <stefanpekez5@gmail.com>
2023-10-07 21:20:05 +0200

    Add swagger ui documentation

---
+++ src/main/java/example/vforecast/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package example.vforecast.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "V-forecast - OpenApi specification"
+        )
+)
+public class OpenApiConfig {
+
+}
--- src/main/java/example/vforecast/controller/CityController.java
+++ src/main/java/example/vforecast/controller/CityController.java
@@ -1,9 +1,9 @@
 package example.vforecast.controller;
 
+import example.vforecast.controller.openapi.CityControllerOpenApi;
 import example.vforecast.dto.city.CityAverageTempGetDto;
 import example.vforecast.dto.city.CityGetDto;
 import example.vforecast.service.CityService;
-import org.springframework.cglib.core.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/cities")
-public class CityController {
+public class CityController implements CityControllerOpenApi {
 
     private final CityService cityService;
 
@@ -35,7 +34,6 @@ public class CityController {
                                                                @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime to,
                                                                @RequestParam Optional<String> cities,
                                                                @RequestParam Optional<String> sort) {
-
         if (cities.isPresent()) {
             return this.cityService.findAverageTemperatures(from, to, cities.get(), sort);
         }
